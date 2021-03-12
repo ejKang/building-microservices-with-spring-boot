0f07340d89bc248eca34e9dd7f605e0bdcfbd751
eunjoo_kang <eunjoo_kang@tmax.co.kr>
2021-03-12 18:58:14 +0900

    add all changes
    seconde step testing

--- product-service/src/main/java/ecommerce/firstproduct/controller/ProductFirstController.java
+++ product-service/src/main/java/ecommerce/firstproduct/controller/ProductFirstController.java
@@ -1,13 +1,22 @@
 package ecommerce.firstproduct.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ecommerce.productservice.entity.Product;
 import ecommerce.productservice.repository.ProductRepository;
@@ -18,6 +27,12 @@ public class ProductFirstController {
     
     @Autowired
     private ProductRepository repo;
+    
+    @Autowired
+    private JmsTemplate jmsTemplate;
+    
+    @Value("${product.jms.destination}")
+    private String jmsQueue;
 
      @PostMapping("/addOne")
      public Product addProduct(@RequestBody Product product	) {
@@ -32,6 +47,28 @@ public class ProductFirstController {
      @GetMapping("/getAll")
      public List<Product> getAllProduct() {
     	 return repo.findAll();
+     }
+     
+     @GetMapping("/sendToCart/{id}")
+     public ResponseEntity<Product> sendToCart(@PathVariable long id) {
+    	 Optional<Product> product = repo.findById(id);
+    	 
+    	 if (!product.isPresent()) {
+    		 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    	 }
+    	 
+    	 ObjectMapper mapper = new ObjectMapper();
+    	 try {
+			String jsonString = mapper.writeValueAsString(product.get());
+			
+			jmsTemplate.convertAndSend(jmsQueue, jsonString);
+			return new ResponseEntity<>(product.get(), HttpStatus.OK);
+		} catch (JsonProcessingException e) {
+			
+			e.printStackTrace();
+			
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
      }
     
 }
--- product-service/src/main/java/ecommerce/productservice/controller/ProductController.java
+++ product-service/src/main/java/ecommerce/productservice/controller/ProductController.java
@@ -1,76 +1,74 @@
 package ecommerce.productservice.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import ecommerce.productservice.entity.Product;
-import ecommerce.productservice.repository.ProductRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ecommerce.productservice.entity.Product;
+import ecommerce.productservice.repository.ProductRepository;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-
+    
     @Autowired
-    private ProductRepository productRepository;
-
+    private ProductRepository repo;
+    
     @Autowired
     private JmsTemplate jmsTemplate;
-
+    
     @Value("${product.jms.destination}")
     private String jmsQueue;
 
-
-    @PostMapping("/addOne")
-    public Product addProduct(@RequestBody Product product) {
-        return productRepository.save(product);
-    }
-
-    @PostMapping("/addList")
-    public List<Product> addProductList(@RequestBody List<Product> products) {
-        return productRepository.saveAll(products);
-    }
-
-    @GetMapping("/getAll")
-    public List<Product> getAllProduct() {
-        return productRepository.findAll();
-    }
-
-    @GetMapping("/info")
-    public String getInfo() {
-        return "product microservice";
-    }
-
-    @GetMapping("/sendToCart/{id}")
-    public ResponseEntity<Product> sendToCart(@PathVariable long id) {
-
-        Optional<Product> product = productRepository.findById(id);
-
-        if (!product.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        try {
-
-            ObjectMapper mapper = new ObjectMapper();
-
-            String jsonInString = mapper.writeValueAsString(product.get());
-            jmsTemplate.convertAndSend(jmsQueue, jsonInString);
-
-            return new ResponseEntity<>(product.get(), HttpStatus.OK);
-
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-    }
-
+     @PostMapping("/addOne")
+     public Product addProduct(@RequestBody Product product	) {
+    	 return repo.save(product);
+     }
+     
+     @PostMapping("/addList")
+     public List<Product> addProductList (@RequestBody List<Product> products) {
+    	 return repo.saveAll(products);
+     }
+     
+     @GetMapping("/getAll")
+     public List<Product> getAllProduct() {
+    	 return repo.findAll();
+     }
+     
+     @GetMapping("/sendToCart/{id}")
+     public ResponseEntity<Product> sendToCart(@PathVariable long id) {
+    	 Optional<Product> product = repo.findById(id);
+    	 
+    	 if (!product.isPresent()) {
+    		 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    	 }
+    	 
+    	 ObjectMapper mapper = new ObjectMapper();
+    	 try {
+			String jsonString = mapper.writeValueAsString(product.get());
+			
+			jmsTemplate.convertAndSend(jmsQueue, jsonString);
+			return new ResponseEntity<>(product.get(), HttpStatus.OK);
+		} catch (JsonProcessingException e) {
+			
+			e.printStackTrace();
+			
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+     }
+    
 }
