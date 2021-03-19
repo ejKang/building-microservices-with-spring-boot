3733ef705c14a680bdd06f54541ec8a8b915e2ae
eunjoo_kang <eunjoo_kang@tmax.co.kr>
2021-03-19 18:04:55 +0900

    add json property annotaion
    Product object values are all empty

--- cart-service/src/main/java/ecommerce/cartservice/entity/Product.java
+++ cart-service/src/main/java/ecommerce/cartservice/entity/Product.java
@@ -7,17 +7,24 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Product {
 
     @Id
+    @JsonProperty("id")
     private Long id;
 
+    @JsonProperty("name")
     private String name;
 
+    @JsonProperty("price")
     private Double price;
 
 }
